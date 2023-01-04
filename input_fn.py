2d23f3bc4652526365290572beed4005167ec191
Devesh <deveshshukla224@gmail.com>
2023-01-04 07:56:49 +0530

    random basics

---
+++ input_fn.py
@@ -0,0 +1,80 @@
+#Type Conversion:
+# weight_in_kg = input("Weight in KG : ")
+# weight_in_pound = (int(weight_in_kg)/0.45359237)
+# print("Weight in Pound : " + str(weight_in_pound))
+
+
+
+#String
+
+#multi line comment
+# mail_template = '''
+# This is a testing mail from support team,
+# Thank You,
+# Support Team
+# '''
+# print(mail_template)
+
+# name = "jennifier"
+# print(name[1:-1])
+
+
+#Fomatted string
+# first= "Devesh"
+# last = "Shukla"
+# message = f'{first} [{last}] is learning Python'
+# print(message)
+
+
+
+#String Methods
+
+# str = "Python for life"
+# print(len(str))
+# print(str.upper())
+# print(str.lower())
+# print(str.capitalize())
+# print(str.find("S")) #if occurance of specified character is not met then -1 will be return
+# print(str.find("O")) # case-sensitive
+# print(str.find('o')) # return first occurance
+# print(str.replace("life","automation")) #.replace(old one , new one)
+# print(str.replace("o", "jj")) # will replace all o with jj
+# is_exist = 'python' in str
+# print(is_exist)
+# is_exist = 'Python' in str
+# print(is_exist)
+
+
+#Math Function
+
+# import math
+# print(math.factorial(5))
+# print(math.fabs(-2.9))
+# print(math.floor(2.9))
+# print(math.ceil(2.9))
+# print(math.pi)
+
+
+#Conditions
+# weather=input("how is weather? ")
+# if(weather=='hot'):
+#     print("Drink Plenty of water")
+# elif(weather=='warm'):
+#     print("Wear sweaters")
+# else:
+#     print("Enjoy")
+
+#comparision
+
+# name ="Devesh Shukla"
+# if(len(name)<3):
+#     print("Name should be greater than 3 charactors")
+# elif(len(name)>50):
+#     print("Name should not be more than 50 charactor long")
+# else:
+#     print("Name looks good")
+
+
+
+
+
